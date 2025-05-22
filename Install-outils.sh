fcf133ade584b5a61ce87d65e6caec6e858c0e7a
virg736 <virginielechene@proton.me>
2025-05-22 14:55:22 +0200

    Update Install-outils.sh
    OSINT - GUIDE

--- Install-outils.sh
+++ Install-outils.sh
@@ -1,32 +1,58 @@
 #!/bin/bash
+
 # -------------------------------------------------------
 # Script d'installation automatique d'outils OSINT
 # Auteur : virg736
 # GitHub : https://github.com/virg736/osint-guide-
 # Licence : MIT
-# Dernière mise à jour : 2025-05-22
+# Dernière MàJ: 2025-05-22
 # -------------------------------------------------------
-
 
 echo "Installation des outils OSINT..."
 
-# Mise à jour et installation des paquets nécessaires
-sudo apt update && sudo apt install -y theharvester git python3-pip  python3-venv
+# -------------------------------------------------------
+# 1. Mise à jour et installation des paquets systèmes
+# -------------------------------------------------------
+sudo apt update && sudo apt install -y \
+git \
+python3-pip \
+python3-venv \
+theharvester
 
-# Création d'un environnement virtuel 
-python3 -m venv venv 
+# -------------------------------------------------------
+# 2. Création d'un environnement Python isolé
+# -------------------------------------------------------
+python3 -m venv venv
 source venv/bin/activate
 
-# dnscan
+# -------------------------------------------------------
+# 3. Installation des dépendances Python manuelles
+# nécessaires pour SpiderFoot
+# -------------------------------------------------------
+pip install \
+dnspython \
+cryptography \
+pyopenssl \
+requests \
+publicsuffixlist \
+networkx \
+beautifulsoup4 \
+phonenumbers
+
+# -------------------------------------------------------
+# 4. Clonage et installation de dnscan
+# -------------------------------------------------------
 git clone https://github.com/rbsec/dnscan.git
 cd dnscan
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 cd ..
 
-# SpiderFoot
+# -------------------------------------------------------
+# 5. Clonage et installation de SpiderFoot
+# -------------------------------------------------------
 git clone https://github.com/smicallef/spiderfoot.git
 cd spiderfoot
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 cd ..
 
 echo "Installation terminée !"
